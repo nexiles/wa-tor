93e10c48e85f973506d320902a2004e802d77e9f
jasmin19 <jasmin.wycislok@nexiles.com>
2014-10-01 14:47:38 +0200

    basic architecture for the application
    closes #4

---
+++ src/main.py
@@ -0,0 +1,75 @@
+def main(rounds):
+  #start_game(rounds) -> void
+  #
+  #:rounds: a number which descibes the rounds simulating
+  #
+  #This is the main funcation of our application, here we start the app and loop the
+  #simulation rounds.
+
+  # 1 Round = 1 time unit (Zeiteinheit)
+
+def create_ocean():
+  #
+  #on each grid point can only be one fish or only one shark
+  #grid is stapled so -> when the shark or fish want to swim on one side he
+  #comes out on the opposite side out again.
+  #
+
+
+def create_fish(id):
+  #create_fish(id) -> a dict
+  #
+  #This function creates a fish objcet which we can place in our ocean.
+  #
+  #The fish object should look like this.
+  #
+  # {
+  #  "id": id,
+  #  "cordinates":
+  # }
+  #
+
+def create_shark(id):
+  #create_shark(id) -> a dict
+  #
+  #This function crea a shark objectwhich we can place in our ocean.
+  #
+  #The shark object should look like this.
+  # {
+  #  "id": id,
+  #   "cordiantes":
+  # }
+  #
+
+
+
+#nfish = Anzahl der Fische am Anfang
+#fbrut = Zahl die ein Fisch exestieren muss bis er ein Nachkommen hat
+
+def fish(nfish, fbrut):
+  #{
+  # "nfish": 100,
+  # "fbrut": 4
+  #}
+
+#if fbrut > 4:
+  #print create_fish
+
+#nshark = Anzahl der Haie am Anfang
+#sbrut = Zahl die ein Hai exestieren muss bis er ein Nachkommen hat
+#fasten = Die Zahl die ein Hai ohne Nahrungs auskommt
+
+def shark(nshark, sbrut, fasten):
+  #{
+  # "nshark": 10,
+  # "sbrut": 5
+  # "fasten": 4
+  #}
+
+#if sbrut > 5:
+   #print create_shark
+
+#for every period, while the shark doesn´t find a fish, he losing an energy point.
+#if the shark find a fish, his energy value increases of the fish enegry.
+#
+#
